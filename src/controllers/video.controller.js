2e2238149c40aedc74522e7692cb4122bd2d52ee
Nikhil <niktemporary69@gmail.com>
2025-11-03 21:45:10 +0530

    Refactor video controller: enhance error handling, add delete and toggle publish status functionalities

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,9 +1,10 @@
-import mongoose from "mongoose";
+import mongoose, {isValidObjectId} from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiErrors.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.service.js";
+import { destroyFromCloudinary, uploadOnCloudinary } from "../utils/cloudinary.service.js";
 import { Video } from "../models/video.model.js" 
 import { ApiResponse } from "../utils/ApiResponse.js";
+
 
 
 const publishVideo = asyncHandler( async(req, res) => {
@@ -19,19 +20,23 @@ const publishVideo = asyncHandler( async(req, res) => {
     const thumbnailPath = req.files?.thumbnail?.[0]?.path
 
     if(!videoFilePath)
-        throw new ApiError(401, "Video File is Required!!")
+        throw new ApiError(400, "Video File is Required!!")
     if (!thumbnailPath)
-        throw new ApiError(401, "Thumbail is required!!")
+        throw new ApiError(400, "Thumbail is required!!")
 
     const videoFile = await uploadOnCloudinary(videoFilePath)
+    if (!videoFile)
+        throw new ApiError(500, "Video upload on cloudinary failed!!")
+
     const thumbnail = await uploadOnCloudinary(thumbnailPath)
+    if (!thumbnail){
+        await destroyFromCloudinary(videoFile.url)
+        throw new ApiError(500, "Thumbnail upload on cloudinary failed!!")
+    }
+
     console.log("Video: ", videoFile);
     console.log("Thumbnail: ", thumbnail);
 
-    if (!videoFile)
-        throw new ApiError(402, "Video upload on cloudinary failed!!")
-    if (!thumbnail)
-        throw new ApiError(402, "Thumbnail upload on cloudinary failed!!")
 
     const video = await Video.create({
         videoFile: videoFile.url,
@@ -43,14 +48,141 @@ const publishVideo = asyncHandler( async(req, res) => {
     })
 
     if (!video)
-        throw new ApiError(501, "Something went wrong while creating video database")
+        throw new ApiError(500, "Something went wrong while creating video database")
 
     return res
-    .status(200)
-    .json( new ApiResponse( 200, video, "Video Uploaded Successfully!!"))
+    .status(201)
+    .json( new ApiResponse( 201, video, "Video Uploaded Successfully!!" ))
 
 })
 
+const getVideoById = asyncHandler( async(req, res) => {
+    const { videoId } = req.params
+
+    if (!videoId)
+        throw new ApiError(400, "VideoId is mising in the url")
+
+    if (!mongoose.isValidObjectId(videoId))
+        throw new ApiError(400, "Video Id is invalid!!")
+
+    const video = await Video.findById(videoId)
+                  .populate("owner", "username avatar") // add username and avatar to the owner object
+                  .lean()  // only fetch neccessary details from db document
+
+    if (!video)
+        throw new ApiError(404, "Video not found!!")
+
+    return res
+    .status(200)
+    .json( new ApiResponse( 200, video, "Video Fetched Successfully!!"))
+})
+
+const updateVideo = asyncHandler( async (req, res) => {
+    const {videoId} = req.params
+    const {title, description} = req.body
+
+    if (!title || !description)
+        throw new ApiError(400, "Both the fields are required!!")
+
+    const { thumbnailPath } = req.file?.path
+    if (!thumbnailPath)
+        throw new ApiError(400, "Thumbnail File is required!!")
+
+    if (!videoId)
+        throw new ApiError(400, "VideoId is mising in the url")
+
+    if (!mongoose.isValidObjectId(videoId))
+        throw new ApiError(400, "Video Id is invalid!!")
+
+    const thumbnail = await uploadOnCloudinary(thumbnailPath);
+
+    if (!thumbnail?.url)
+        throw new ApiError(500, "Uploading of thumbnail on cloudinary failed!!")
+
+    const video = await Video.findById(videoId)
+    if (!video)
+        throw new ApiError(404, "Video File is not Found!!")
+
+    if (video.owner.toString() !== req.user_id.toString())
+        throw new ApiError(403, "You are not authozied to update video!!")
+
+    const oldThumbnail = video.thumbnail
+
+    video.thumbnail = thumbnail.url;
+    video.title = title;
+    video.description = description;
+    await video.save({validateBeforeSave: false})
+
+    const deletionResponse = await destroyFromCloudinary(oldThumbnail)
+    console.log("Response: ", deletionResponse);
+
+    return res
+    .status(200)
+    .json( new ApiResponse( 200, video, "Video details Updated!!" ))
+})
+
+// todo work on it seriously
+const deleteVideo = asyncHandler( async (req, res) => {
+    const { videoId } = req.params
+
+    if (!videoId)
+        throw new ApiError(400, "VideoId is mising in the url.")
+
+    if (!mongoose.isValidObjectId(videoId))
+        throw new ApiError(400, "Video Id is invalid!!")
+
+    const video = await Video.findById(videoId)
+
+    if (!video)
+        throw new ApiError(500, "Something went wrong while extracting video document from database")
+
+    if (video.owner.toString() !== req.user_id.toString())
+        throw new ApiError(403, "You are not authozied to delete video!!")
+    
+    const videoUrl = video.videoFile
+    const thumbnailUrl = video.thumbnail
+    
+    await destroyFromCloudinary(videoUrl)
+    await destroyFromCloudinary(thumbnailUrl)
+    await Video.findByIdAndDelete(videoId)
+
+    return res
+    .status(200)
+    .json( new ApiResponse (200, {}, "Video Deleted Successfully!!"))
+    
+})
+
+const togglePublishStatus = asyncHandler( async( req, res ) => {
+    const {videoId} = req.params
+
+    if (!videoId)
+        throw new ApiError(400, "VideoId is mising in the url")
+
+    if (!mongoose.isValidObjectId(videoId))
+        throw new ApiError(400, "Video Id is invalid!!")
+
+    const video = await Video.findById(videoId)
+
+    if (!video)
+        throw new ApiError(404, "Video Not Found!!")
+    
+    if (video.owner.toString() !== req.user_id.toString())
+        throw new ApiError(403, "You are not authozied to toggle publish status of video!!")
+
+    const publishStatus = video.isPublished
+    video.isPublished = !publishStatus
+
+    await video.save({validateBeforeSave: false})
+
+    return res
+    .status(200)
+    .json( new ApiResponse(200, {isPublished: video.isPublished}, "Publish Status TOggled Successfully!!"))
+})
+
 export {
-    publishVideo
+    publishVideo,
+    getVideoById,
+    updateVideo,
+    deleteVideo,
+    togglePublishStatus,
 }
